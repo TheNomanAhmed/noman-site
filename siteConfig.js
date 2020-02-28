1ca9eab6aeb515f007b2ac8fdd4ef1fd3b7370ce
TheNomanAhmed <nomanahmed478@gmail.com>
2020-02-28 14:42:11 +0200

    v 3.0

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Noman Ahmed Khan",
   shortName: "NomanKhan",
   description:
-    "Front-End Developer working with startups to create great products & brands.",
+    "UI/UX Designer working with startups to create great products & brands.",
   url: "https://www.TheNomanAhmed.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Noman Ahmed Khan",
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { graphql, StaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostCard from "../components/postCard"
-import ReactTypingEffect from "react-typing-effect"
 
 // import "../utils/global.scss"
 import "../utils/normalize.css"
@@ -16,36 +15,30 @@ const BlogIndex = ({ data }, location) => {
   let postCounter = 0
 
   return (
-    <div>
-      <Layout title={siteTitle}>
-        <SEO title=" " keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-        {/* <Bio /> */}
+    <Layout title={siteTitle}>
+      <SEO title=" " keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      {/* <Bio /> */}
+      {data.site.siteMetadata.description && (
         <header className="page-head">
           <h2 className="page-head-title">
-            <ReactTypingEffect
-              staticText="UI/UX Designer who loves to"
-              text={["code.", "design."]}
-              speed={50} //text=["Hello.", "World!"]
-              typingDelay={200}
-              eraseDelay={5000}
-            />
+            {data.site.siteMetadata.description}
           </h2>
         </header>
-        <div className="post-feed">
-          {posts.map(({ node }) => {
-            postCounter++
-            return (
-              <PostCard
-                key={node.fields.slug}
-                count={postCounter}
-                node={node}
-                postClass={`post`}
-              />
-            )
-          })}
-        </div>
-      </Layout>
-    </div>
+      )}
+      <div className="post-feed">
+        {posts.map(({ node }) => {
+          postCounter++
+          return (
+            <PostCard
+              key={node.fields.slug}
+              count={postCounter}
+              node={node}
+              postClass={`post`}
+            />
+          )
+        })}
+      </div>
+    </Layout>
   )
 }
 
